6dbe19a24827e5d20f0e95682bddf90ae9b1c95e
124sidgit <sharmayas2002@gmail.com>
2021-12-28 17:46:00 +0530

    First code

---
+++ python-c/qrcode.py
@@ -0,0 +1,12 @@
+#import qrcode module
+import qrcode
+
+#Make a variable data in which we can store our information which we want to convert in qrcode
+#And put the link of any website
+data="Name=Sid\nDOB=00/00/0000\nCountry=Globe"
+
+#Here the data is converted into qrcode
+img=qrcode.make(data)
+
+#And here it is save in the address given by us
+img.save("C:/Users/sis-s/Desktop")
